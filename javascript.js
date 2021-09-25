5470d8a76432c5ab9ef422748917bdfe9aefcfd7
Aleksejs Pavlovs <71398178+prokurists@users.noreply.github.com>
2021-09-25 08:59:58 +0300

    updated javascript.js
    code updated. now with test. not working yet.

--- javascript.js
+++ javascript.js
@@ -1,51 +1,62 @@
-                        module.exports = { 
-                            //param A : array of integers
-                            //param B : array of integers
-                            //return an integer
-                            canCompleteCircuit : function(A, B){
+function checkCode() {
+    const testVar = {
+        firstTest: {
+            A: [0],
+            B: [0]
+        },
+        secondTest: {
+            A: [2, 3, 4, 4, 6],
+            B: [4, 5, 6, 1, 3]
+        },
+        thirdTest: {
+            A: [4],
+            B: [5]
+        },
+    }
 
-                            let tank = 0;
-                            let startTank = 0;
-                            let s = 0;
-                            let total = 0;
-
-                            do {
-                                startTank = s;
-                                s++;
-
-                            } while (A[s] - B[s] < 0)
-                            for (let i = startTank; i < A.length; i++){
-                                tank = tank + A[i] - B[i];
-
-                                if (tank < 0) {
-                                    total = tank;
-                                } else { 
-                                    if (i === A.length) {
-                                    for (let g = 0; g < startTank; g++){
-                                        tank = tank + A[g] - B[g];
-
-                                        if (tank < 0) {
-                                            total = tank;
-                                        }
-                                        total = tank;
-                                    }
-                                }
-                            }
-                            }
-
-                            if (total >= 0){
-                                return A[startTank];
-                            } else {
-                                return -1
-                            }
-                                
+    Object.values(testVar).forEach(val => 
+        console.log(val)
+        );
 
 
-                    }
-
-                        };
+    const para = document.createElement("p");
+    const title = document.createElement("h1");
+    const titleNode = document.createTextNode("H1 TEXT")
+    const node = document.createTextNode("Paragraph text");
+    title.appendChild(titleNode);
+    para.appendChild(title);
+    para.appendChild(node);
 
 
 
+    const element = document.getElementById("main");
+    element.appendChild(para);
+  }
 
-                        
+
+  const task = {
+    canCompleteCircuit : function (A, B){
+        
+        let total = 0;
+        let start = 0;
+        let tank = 0
+    
+    for (let i=0; i < A.length; i++){
+
+        total+=A[i]-B[i];
+        tank = tank + A[i]-B[i];
+
+        if (tank < 0){
+            start = i + 1;
+            tank = 0;
+        }
+    
+    }
+    
+    if (total < 0 ){
+        return -1;
+    } 
+    return start;
+        
+    }
+  }
